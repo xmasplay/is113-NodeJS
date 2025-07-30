bb69f316c773b1a3cdc6b69a47a99c5a0305b308
xmasplay <yimeng.lau@gmail.com>
2025-07-30 16:28:54 +0800

    Create server.js

---
+++ week1/server.js
@@ -0,0 +1,19 @@
+// Import the http module to create a server
+const http = require('http');
+
+// Define the server
+const server = http.createServer((req, res) => {
+ // Set the response header to indicate HTML content
+ res.writeHead(200, { 'Content-Type': 'text/html' });
+
+ // Send the "Hello, World!" message to the browser
+ res.end('<h1>Hello, World!</h1>');
+});
+
+console.log("This is from the fork");
+
+// The server listens on port 3000
+const port = 8000;
+server.listen(port, () => {
+ console.log(`Server is running at http://localhost:${port}`);
+});
